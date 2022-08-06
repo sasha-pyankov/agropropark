2d5ae61a51c3d024183ad7f563d748d462cdefd9
sasha-pyankov <sashapyankov86@gmail.com>
2022-08-06 11:39:58 +0300

    добавил новую секцию

--- #source/js/burger.js
+++ #source/js/burger.js
@@ -1,6 +1,6 @@
 /* бургер-меню */
-const iconMenu = document.querySelector('.top-menu-haader__burger-icon');
-const headerMenu = document.querySelector('.menu-haader');
+const iconMenu = document.querySelector('.top-menu-header__burger-icon');
+const headerMenu = document.querySelector('.menu-header');
 if (iconMenu) {
   iconMenu.addEventListener("click", function (e) {
     document.body.classList.toggle('_lock');
